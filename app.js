f96c4398355c0406b589b81f0817f3e3dbb2f450
Stephen Jorge Arambulo <stephja@stephens-mbp.mynet>
2020-04-27 02:01:00 +0100

    added functionality when inputed letters are correct match or not with hangman word

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const words = ["apple", "watermelon", "kiwi", "a"]
 let randomWordPicker = Math.floor(Math.random() * words.length);
 let selectedWord = words[randomWordPicker];
 let underscoreLength = [];
-// console.log(selectedWord);
+console.log(selectedWord);
+
+let charMatching = [];
+let notMatching = [];
+
 
 let generateUnderscoreLength = () => {
     for (let i = 0; i < selectedWord.length; i++) {
@@ -13,3 +17,17 @@ let generateUnderscoreLength = () => {
     return underscoreLength;
 }
 console.log(generateUnderscoreLength());
+
+
+document.addEventListener('keypress', (event) => {
+    let keycode = event.keyCode;
+    let keyword = String.fromCharCode(event.keyCode);
+    
+    if (selectedWord.indexOf(keyword) > -1) {
+        charMatching.push(keyword);
+        console.log(charMatching);
+    } else {
+        notMatching.push(keyword);
+        console.log(notMatching);
+    }
+});
